8ceaa4125035ca02193861f6820fe45da8fe8593
DanielAristy <danielariaty@gmail.com>
2019-05-21 00:00:48 -0500

    Guia completa de React

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
-/*Aqui es donde se importan nuestros proyectos */
-import React from 'react'
-import ReactDOM from 'react-dom'
-import App from './App'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import './index.css';
 
-ReactDOM.render(<App />, document.getElementById('root'))
+ReactDOM.render(<App />, document.getElementById('root'));
